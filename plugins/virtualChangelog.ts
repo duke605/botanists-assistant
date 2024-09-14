7138d51483bb3f93eafa874a0ce38aab9987488e
duke605 <cmdenslow14@hotmail.com>
2024-09-14 17:48:12 -0500

    Corrected timezone of changelog updates

--- plugins/virtualChangelog.ts
+++ plugins/virtualChangelog.ts
@@ -3,9 +3,13 @@ import fs from 'node:fs';
 import { Plugin } from 'vite';
 import dayjs from 'dayjs';
 import customParseFormat from 'dayjs/plugin/customParseFormat';
+import utc from 'dayjs/plugin/utc';
+import timezone from 'dayjs/plugin/timezone';
 import * as YAML from 'yaml';
 
 dayjs.extend(customParseFormat);
+dayjs.extend(utc);
+dayjs.extend(timezone);
 
 export default (): Plugin => {
   const name = 'virtual-changelog';
@@ -22,7 +26,7 @@ export default (): Plugin => {
       if (id !== resolvedPluginId) return;
       const data = fs.readFileSync(changelogPath);
       const code = Object.entries(YAML.parse(data.toString())).map(([ date, changes ]) =>
-        `{date: dayjs(${dayjs(date, 'YYYY-MM-DD').unix() * 1000}), changes: ${JSON.stringify(changes)}}`
+        `{date: dayjs(${dayjs.tz(date, 'YYYY-MM-DD', 'America/Winnipeg').unix() * 1000}), changes: ${JSON.stringify(changes)}}`
       ).join(',\n  ')
 
       return `import dayjs from 'dayjs';export default [${code}];`;
